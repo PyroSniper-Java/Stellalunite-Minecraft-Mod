6c3fe03efc65607cb1bb14a43f62f29d2a078151
PyroSniper-Java <k.matches@hotmail.com>
2024-09-06 16:30:24 -0400

    I changed/added some things

--- src/main/java/com/pyrocoder/stellalunitemod/item/custom/StellaluniteBone.java
+++ src/main/java/com/pyrocoder/stellalunitemod/item/custom/StellaluniteBone.java
@@ -1,15 +1,12 @@
 package com.pyrocoder.stellalunitemod.item.custom;
 
-import com.pyrocoder.stellalunitemod.item.ModItems;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class StellaluniteBone extends Item {
     public StellaluniteBone(Properties pProperties) {
@@ -18,10 +15,16 @@ public class StellaluniteBone extends Item {
 
     @Override
     public InteractionResult interactLivingEntity(ItemStack pStack, Player pPlayer, LivingEntity pInteractionTarget, InteractionHand pUsedHand) {
-        if(pInteractionTarget = Wolf) {
 
-
+        if(pInteractionTarget instanceof Wolf wolf){
+            if(!wolf.isAngry() && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(wolf, pPlayer)){
+                wolf.tame(pPlayer);
+                wolf.setTarget(null);
+                wolf.setOrderedToSit(true);
+                wolf.getNavigation().stop();
+                wolf.level().broadcastEntityEvent(wolf, (byte)7);
+            }
         }
-        return InteractionResult.SUCCESS;
+        return null;
     }
 }
