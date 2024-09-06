d503d581d6fe6b5c36b272646b9806f2a2e68b8b
PyroSniper-Java <k.matches@hotmail.com>
2024-09-06 15:51:23 -0400

    I changed/added some things

--- src/main/java/com/pyrocoder/stellalunitemod/StellaluniteMod.java
+++ src/main/java/com/pyrocoder/stellalunitemod/StellaluniteMod.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
--- src/main/java/com/pyrocoder/stellalunitemod/item/custom/StellaluniteBone.java
+++ src/main/java/com/pyrocoder/stellalunitemod/item/custom/StellaluniteBone.java
@@ -1,11 +1,27 @@
 package com.pyrocoder.stellalunitemod.item.custom;
 
+import com.pyrocoder.stellalunitemod.item.ModItems;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.animal.Wolf;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class StellaluniteBone extends Item {
     public StellaluniteBone(Properties pProperties) {
         super(pProperties);
     }
 
+    @Override
+    public InteractionResult interactLivingEntity(ItemStack pStack, Player pPlayer, LivingEntity pInteractionTarget, InteractionHand pUsedHand) {
+        if(pInteractionTarget = Wolf) {
 
+
+        }
+        return InteractionResult.SUCCESS;
+    }
 }
